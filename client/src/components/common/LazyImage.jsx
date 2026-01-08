f45ec5a5c5b4b668a10c271c313311f9dcb43924
ayseeaydin <ayssee.aydinn.04@gmail.com>
2026-01-08 19:04:44 +0300

    chore: remove LazyImage component and associated styles to eliminate unused code

--- client/src/components/common/LazyImage.jsx
+++
@@ -1,5 +0,0 @@
-// LazyImage component was removed to reduce unused code.
-// Keep a tiny stub so any accidental imports won't break the build.
-const LazyImage = () => null
-
-export default LazyImage
